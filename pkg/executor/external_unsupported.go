9f315e95a7c8095b604766b6604adee6db05c956
李宏杰 <lihongjie0209@gmail.com>
2026-08-13 17:57:37 +0800

    fix: support scheduler cross compilation

---
+++ pkg/executor/external_unsupported.go
@@ -0,0 +1,26 @@
+//go:build !unix
+
+package executor
+
+import (
+	"context"
+	"errors"
+)
+
+type ScriptOptions struct{ Languages []string }
+
+func ScriptHandler(ScriptOptions) Handler {
+	return func(context.Context, Task) error {
+		return errors.New("script execution is not supported on this operating system")
+	}
+}
+
+type DockerOptions struct {
+	Binary string
+}
+
+func DockerHandler(DockerOptions) Handler {
+	return func(context.Context, Task) error {
+		return errors.New("Docker execution is not supported on this operating system")
+	}
+}
